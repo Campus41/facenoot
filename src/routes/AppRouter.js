3d819215b23988703d1dd89c053e9d14941148f4
Campus41 <campus41@gmail.com>
2023-09-11 15:29:07 +0300

    pages

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { UsersList, UserDetails } from '../components/users/';
 import { UserPosts, UserPost } from '../components/posts';
 import { UserAlbums, UserAlbum } from '../components/albums';
